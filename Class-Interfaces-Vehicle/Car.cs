75a719680e53013c1d6dd90013faa57627526fde
ValterFrancisco <ValterFrancisco@PT-AL-TEC005>
2022-03-17 17:55:59 +0000

    Add project files.

---
+++ Class-Interfaces-Vehicle/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Interfaces_Vehicle
+{
+    internal class Car : IVehicle
+    {
+        int gasoline { get; set; }
+
+        public Car(int gasoline)
+        {
+            this.gasoline = gasoline;
+        }
+
+        public bool FuelLevel()
+        {
+            if (this.gasoline > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving!");
+            this.gasoline = 0;
+        }
+
+        public bool Refuel(int gasoline)
+        {
+            if (gasoline > 0)
+            {
+                this.gasoline = this.gasoline + gasoline;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Class-Interfaces-Vehicle/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Class_Interfaces_Vehicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumber = false;
+            int gasoline = 0;
+
+            Car car = new Car(gasoline);
+            while (true)
+            {
+                do
+                {
+                    Console.WriteLine("Please fuel the car! (Insert a number)");
+
+                    string input = Console.ReadLine();
+                    try
+                    {
+                        input = input.Trim();
+                        gasoline = Convert.ToInt32(input);
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please insert a valid input");
+                    }
+                    if (gasoline > 0)
+                    {
+                        isNumber = true;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please insert a valid input");
+                    }
+                }
+                while (isNumber == false);
+
+                car.Drive();
+                bool refuelVerification = true;
+
+                do
+                {
+                    Console.WriteLine("Do you want to refuel? (y/n)");
+
+                    string refuel = Console.ReadLine();
+                    refuel.Trim();
+                    refuel.ToLower();
+
+
+
+                    if (refuel == "y")
+                    {
+                        refuelVerification = false;
+                    }
+                    else if (refuel == "n")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please insert a valid input");
+                    }
+                }
+                while (refuelVerification == true);
+            }
+        }
+    }
+}
